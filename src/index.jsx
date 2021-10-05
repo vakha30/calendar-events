0574773910b194e04fc2dac41dfe0ec93d5956b6
Vakha <gvardee95@gmail.com>
2021-10-05 21:09:19 +0300

    Сделал шапку сайта

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./components/App";
@@ -8,7 +9,9 @@ import store from "./redux/configureStore";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
