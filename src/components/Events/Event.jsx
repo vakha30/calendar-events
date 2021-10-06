fa74c064af4d19f357dbb4578ab1a9569f921dbb
Vakha <gvardee95@gmail.com>
2021-10-06 20:07:27 +0300

    Сверстал страницу событий, сделал адаптив

--- src/components/Events/Event.jsx
+++ src/components/Events/Event.jsx
@@ -1,25 +1,31 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import cl from "./events.module.css";
+import { Link } from "react-router-dom";
 
 const Event = React.memo(function ({ id, title, image, date }) {
   const [dateObj, setDateObj] = useState({});
   useEffect(() => {
     const newDate = new Date(date);
     const obj = {
+      day: newDate.getDay(),
       month: newDate.getMonth(),
       year: newDate.getFullYear(),
+      day2: newDate.getUTCDay(),
     };
     setDateObj(obj);
   }, [date]);
 
   return (
     <div className={cl.event}>
-      <h1>{title}</h1>
-      <img src={image} alt="" />
-      <h3>
-        {dateObj.month} <span>{dateObj.year}</span>
-      </h3>
+      <div className={cl.eventHeader}>
+        <h3>{title.substr(0, 10)}</h3>
+        <Link to={`/event/${id}`}>Больше</Link>
+      </div>
+      <div className={cl.eventImg}>
+        <img src={image} alt={title} />
+      </div>
+      <div className={cl.eventDate}>{`${dateObj.day2}.${dateObj.month}.${dateObj.year}.`}</div>
     </div>
   );
 });
